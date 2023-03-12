6375b85b7590ecdae30d35e0f75d943084ee87c8
ke_An <nkalk192002@gmail.com>
2023-03-12 23:29:35 +0100

    final demo with network layer (peer to peer using socket)

---
+++ player1/peer.c
@@ -0,0 +1,252 @@
+// Libs for system layer (IPC part)
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <pthread.h>
+#include <iconv.h>
+
+// Libs for network layer (p2p part)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/select.h>
+
+// constants for IPC part
+#define MAX 100
+#define COOR_MESSAGE_TYPE 1
+#define FROM_PY_TO_C 2
+#define FROM_C_TO_PY 3
+#define KEY 192002
+
+//constants for p2p part
+#define PORT 4444
+#define name "player_1"
+#define BUFFER_SIZE 1024
+
+//message structure to put into ipc message queue 
+typedef struct message {
+    long message_type;
+    char message_body[100];
+}message;
+
+// System layer. IPC using message queue to communicate between two processes.
+void send_msg_to_python_process(char* msg);
+
+char *read_msg_from_python_process();
+
+// Network layer. P2P using socket to communicate with others peers in network.
+void config_peer(int* server_fd, struct sockaddr_in* address);
+void *send_to_peer();
+void receive_from_peer(int server_fd);
+void *receive_thread(void *server_fd);
+
+int remove_enter_in_buffer(char* buffer);
+
+int main() {
+
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    config_peer(&server_fd, &address);
+
+    // Each thread has its identifier.
+    pthread_t t1;
+    pthread_t t2;
+
+    pthread_create(&t1, NULL, send_to_peer, NULL);
+    pthread_create(&t2, NULL, receive_thread, &server_fd);
+
+    pthread_join(t2, NULL);
+    pthread_join(t1, NULL);    
+
+    return 0;
+}
+
+void config_peer(int* server_fd, struct sockaddr_in* address) {
+    //create sockfd for peer. In p2p architecture each node can be considered as server as well as client.
+    if( ( *server_fd = socket(AF_INET, SOCK_STREAM, 0) ) == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    //create address structure for peer
+    address->sin_family = AF_INET;
+    address->sin_addr.s_addr = INADDR_ANY;
+    address->sin_port = htons(PORT);
+
+    printf("IP address is: %s\n", inet_ntoa(address->sin_addr));
+    printf("port is: %d\n", (int)ntohs(address->sin_port));
+
+    //bind address structure to peer
+    if(bind(*server_fd, (struct sockaddr*)address, sizeof(*address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    //listen for the connection
+    if(listen(*server_fd, 5) < 0) {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void *send_to_peer() {
+    while(1) {
+        // printf("\nstart read msg from python process\n");
+        // char* hello = read_msg_from_python_process();
+        // printf("\n%s\n", hello);
+        char buffer__[2000] = {0};
+        int PORT_to_send; // port of peer of player 2
+        printf("Enter the port to send message:");
+        scanf("%d", &PORT_to_send);
+
+        int sock = 0, valread;
+        struct sockaddr_in serv_addr;
+    
+        if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+            printf("\nsocket creation error\n");
+            exit(EXIT_FAILURE);
+        }
+
+        serv_addr.sin_addr.s_addr = INADDR_ANY;
+        serv_addr.sin_port = htons(PORT_to_send);
+        serv_addr.sin_family = AF_INET;
+
+        if(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+            printf("\nconnection failed\n");
+            exit(EXIT_FAILURE);
+        }
+        printf("\nconnect sucessfully, start reading msg from python process\n");
+        char* buffer = read_msg_from_python_process();
+
+        // sprintf(buffer__, "%s[PORT:%d] says: %s", name, PORT, buffer__);
+        if(send(sock, buffer, strlen(buffer) + 1, 0) == -1) {
+            perror("sending failure");
+            exit(EXIT_FAILURE);
+        }
+        printf("\nmessage sent\n");
+        close(sock);
+    }
+}
+
+void *receive_thread(void *server_fd) {
+    int s_fd = *((int*)server_fd);
+    while(1) {
+        sleep(2);
+        receive_from_peer(s_fd);
+    }
+}
+
+void receive_from_peer(int server_fd) {
+    struct sockaddr_in address;
+    int valread;
+    char buffer[2000] = {0};
+    int addrlen = sizeof(address);
+    fd_set current_sockets, ready_sockets;
+
+    //Initialize my current set
+    FD_ZERO(&current_sockets);
+    FD_SET(server_fd, &current_sockets);
+    int k = 0;
+    while (1)
+    {
+        k++;
+        ready_sockets = current_sockets;
+
+        if (select(FD_SETSIZE, &ready_sockets, NULL, NULL, NULL) < 0)
+        {
+            perror("Error");
+            exit(EXIT_FAILURE);
+        }
+
+        for (int i = 0; i < FD_SETSIZE; i++)
+        {
+            if (FD_ISSET(i, &ready_sockets))
+            {
+
+                if (i == server_fd)
+                {
+                    int client_socket;
+
+                    if ((client_socket = accept(server_fd, (struct sockaddr *)&address,
+                                                (socklen_t *)&addrlen)) < 0)
+                    {
+                        perror("accept");
+                        exit(EXIT_FAILURE);
+                    }
+                    FD_SET(client_socket, &current_sockets);
+                }
+                else
+                {
+                    valread = recv(i, buffer, sizeof(buffer), 0);
+                    printf("\n%s\n", buffer);
+                    // char buffer__[1024];
+                    // printf("\nenter the coor: ");
+                    // scanf("%s", buffer__);
+                    // send_msg_to_python_process(buffer__);
+                    FD_CLR(i, &current_sockets);
+                }
+            }
+        }
+
+        if (k == (FD_SETSIZE * 2))
+            break;
+    }
+}
+
+void send_msg_to_python_process(char* buffer) {
+    //Open the message queue to communicate to process python. The message_queue has its integer identifier
+    int message_queueID = msgget(KEY, 0666 | IPC_CREAT);
+    printf("\nmessage queue id: %d\n", message_queueID);
+
+    message msg;
+    msg.message_type = FROM_C_TO_PY;
+    buffer[strlen(buffer)] = '\n';
+    strncpy(msg.message_body, buffer, strlen(buffer) + 1);
+
+    if(msgsnd(message_queueID, &msg, sizeof(msg), 0) == -1) {
+        perror("send msg failed to python process");
+    }
+}
+
+char *read_msg_from_python_process() {
+    //Open the message queue to communicate to process python. The message_queue has its integer identifier
+    int message_queueID = msgget(KEY, 0666 | IPC_CREAT);
+    printf("\nmessage queue id: %d\n", message_queueID);
+
+
+    message msg;
+
+    // Receive the coor message type
+    msg.message_type = FROM_PY_TO_C; 
+
+    if(msgrcv(message_queueID, &msg, sizeof(msg), msg.message_type, 0) == -1) {
+        perror("read msg failed from python process");
+    }
+    
+    char* buffer = (char*)calloc(BUFFER_SIZE, 1);
+
+    strncpy(buffer, msg.message_body, sizeof(msg.message_body));
+    remove_enter_in_buffer(buffer);
+    printf("\nlen: %d, msg: %s\n",strlen(buffer), buffer);
+
+    return buffer;
+}
+
+int remove_enter_in_buffer(char* buffer) {
+    /*
+    replace '\n' in buffer by '\0'
+    Params: buffer
+    Return: the len of buffer from beginning to '\0'
+    */
+    int k;
+    for(k = 0; k < BUFFER_SIZE; k++) {
+        if(buffer[k] == '\n') {
+            buffer[k] = '\0';
+            break;
+        }
+    }
+    return k;
+}
---
+++ player2/game.py
@@ -0,0 +1,153 @@
+import pygame as pg
+import sysv_ipc
+import sys
+import threading
+import os
+import re
+
+# Constants
+MAX = 100
+KEY = 192001
+COOR_MESSAGE_TYPE = 1
+
+FROM_PY_TO_C = 2
+FROM_C_TO_PY = 3
+
+class ReadWrite:
+
+    def __init__(self):
+        self.message_queue = sysv_ipc.MessageQueue(KEY, sysv_ipc.IPC_CREAT)
+        self.message = None
+        print(self.message_queue)
+
+    def send_message(self, message):
+        self.message_queue.send(message.encode(), type=FROM_PY_TO_C)
+
+
+    def read_message(self):
+        self.message = self.message_queue.receive(type=FROM_C_TO_PY)
+        self.message = self.decode_and_clean_message()
+        
+
+    def decode_and_clean_message(self):
+        temp_list = list(self.message)
+        temp_list[0] = self.message[0].decode(sys.getdefaultencoding(), errors='ignore')
+        temp_list[0] = temp_list[0].split('\n')[0]
+        temp_list[0] = temp_list[0].rstrip('\0')
+        # print(temp_list[0])
+        temp_list[1] = self.message[1]
+        return tuple(temp_list)
+        
+    def get_coordinates(self):
+        numbers = []
+        pattern = r'\d+'
+        for word in self.message[0].split():
+            matches = re.findall(pattern, word)
+            # if word.isdigit():
+            if matches:
+                print(word)
+                numbers.append(int(float(word)))
+        return numbers
+    
+    def get_message_text(self): return self.message[0]
+
+
+class Character:
+
+    def __init__(self, speed, color):
+        self.image = pg.Surface((10, 10))
+        self.image.set_colorkey((12,34,56))
+        self.image.fill((12,34,56))
+        pg.draw.circle(self.image, color, (50, 50), 5)
+        self.rect = self.image.get_rect()
+        self.color = color
+        self.target_access = False
+
+        self.pos = pg.Vector2(50, 50)
+        self.set_target((50, 50))
+        self.speed = speed
+
+    def set_target(self, pos):
+        self.target = pg.Vector2(pos)
+
+    def get_target_access(self): return self.target_access
+
+    def set_target_access(self, isAccess: bool): self.target_access = isAccess
+
+    def draw(self,screen):
+        screen.fill((0, 0, 0))
+        pg.draw.circle(screen, self.color , (self.pos[0], self.pos[1]), 5)
+        pg.display.update()
+
+    def update(self):
+        move = self.target - self.pos
+        move_length = move.length()
+
+        if move_length < self.speed:
+            self.pos = self.target
+            self.set_target_access(True)
+
+        elif move_length != 0:
+            move.normalize_ip()
+            move = move * self.speed
+            self.pos += move
+
+        self.rect.topleft = list(int(v) for v in self.pos)
+
+def run_game(character: Character, read_write: ReadWrite, screen, clock):
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+            
+            if event.type == pg.MOUSEBUTTONDOWN:
+                character.set_target(pg.mouse.get_pos())
+                tar = f"x={character.target[0]} y={character.target[1]}"
+                read_write.send_message(tar)
+
+            if event.type==pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    pg.quit()
+        character.update()
+        screen.fill((20, 20, 20))
+        character.draw(screen)
+        pg.display.flip()
+        clock.tick(60)
+
+def run_read_write(read_write: ReadWrite, character: Character):
+    quit = False
+    
+    while not quit:
+        read_write.read_message()
+        msg_text = read_write.get_message_text()
+        if msg_text:
+            print(msg_text)
+        target = read_write.get_coordinates()
+        print(target)
+        if target:  
+            print(target)
+            character.set_target((target[0], target[1]))
+
+def main():
+    # Pygame configuration
+    pg.init()
+    screen = pg.display.set_mode((500, 500))
+    clock = pg.time.Clock()
+
+    # Create the agents for each thread
+    # The character is for running the game and the reader is for running the reader
+    character = Character(1.5, pg.Color("white"))
+    read_write = ReadWrite()
+
+    thread1 = threading.Thread(target=run_read_write, args=(read_write, character))
+    thread2 = threading.Thread(target=run_game, args=(character, read_write, screen, clock))
+
+    thread1.start()
+    thread2.start()
+
+    thread1.join()
+    thread2.join()
+
+
+if __name__ == '__main__':
+    main()
